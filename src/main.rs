9f6f4ddb8854f3a976e3f2a8f0dec13c752eeda9
Yury Gribkov <yury.gribkov@gmail.com>
2021-10-23 18:33:03 -0700

    Minimal svc impl with a state

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
+
+#![deny(warnings)]
+
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+
 use hyper::service::{make_service_fn, service_fn};
-
-async fn get_by_key() -> Result<String, Infallible> {
-    Ok("".into())
-}
-
-async fn get_from_cache() -> Result<String, Infallible> {
-    Ok("Hello world!".into())
-}
-
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    // let a = get_from_cache().map(|v| { v + ", World!"})
-    match get_from_cache().await {
-        Result::Ok(s) => Ok(Response::new(s.into())),
-        _ => Ok(Response::new("Not found!".into()))
-    }   
-}
+use hyper::{Body, Error, Response, Server};
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    pretty_env_logger::init();
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    // For the most basic of state, we just share a counter, that increments
+    // with each request, and we send its value back in the response.
+    let counter = Arc::new(AtomicUsize::new(0));
+
+    // The closure inside `make_service_fn` is run for each connection,
+    // creating a 'service' to handle requests for that specific connection.
+    let make_service = make_service_fn(move |_| {
+        // While the state was moved into the make_service closure,
+        // we need to clone it here because this closure is called
+        // once for every connection.
+        //
+        // Each connection could send multiple requests, so
+        // the `Service` needs a clone to handle later requests.
+        let counter = counter.clone();
+
+        async move {
+            // This is the `Service` that will handle the connection.
+            // `service_fn` is a helper to convert a function that
+            // returns a Response into a `Service`.
+            Ok::<_, Error>(service_fn(move |_req| {
+                // Get the current count, and also increment by 1, in a single
+                // atomic operation.
+                let count = counter.fetch_add(1, Ordering::AcqRel);
+                async move { Ok::<_, Error>(Response::new(Body::from(format!("Request #{}", count)))) }
+            }))
+        }
     });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(make_service);
 
-    // Run this server for... forever!
+    println!("Listening on http://{}", addr);
+
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
 }
-
